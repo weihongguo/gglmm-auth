241fd3abd364cbfa9e8b1d1a11d913d9029bf7e6
weihongguo <35117668@qq.com>
2020-05-26 23:17:14 +0800

    config

--- config.go
+++ config.go
@@ -10,3 +10,24 @@ var (
 	// ErrAuthType --
 	ErrAuthType = errors.New("Authorization类型错误")
 )
+
+// ConfigJWT --
+type ConfigJWT struct {
+	Expires int64
+	Secret  string
+}
+
+// Check --
+func (config *ConfigJWT) Check(cmd string) bool {
+	if cmd == "all" || cmd == "write" {
+		if config.Expires <= 0 {
+			return false
+		}
+	}
+	if cmd == "all" || cmd == "read" {
+		if config.Secret == "" {
+			return false
+		}
+	}
+	return true
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ replace github.com/weihongguo/gglmm => ../gglmm
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/weihongguo/gglmm v0.0.0-00010101000000-000000000000
+	github.com/weihongguo/gglmm v0.0.0-20200225064623-73efc6160d28
 )
